1be04b347cccbbad722834f86ce8c5194e2320a2
Tuomas Siipola <siiptuo@kapsi.fi>
2016-01-13 13:54:19 +0200

    Display activity durations

--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 
 require 'database.php';
 
+function formatDuration($start, $end) {
+    $diff = $end->diff($start);
+    if ($diff->h === 0) {
+        return $diff->i === 0 ? "{$diff->s}s" : "{$diff->i}min";
+    } else {
+        return "{$diff->h}h {$diff->i}min";
+    }
+}
+
 $currentActivity = null;
 $activities = [];
 foreach ($pdo->query('SELECT * FROM activity ORDER BY started_at DESC') as $row) {
     $row['started_at'] = new DateTime($row['started_at']);
     if (!isset($row['finished_at'])) {
+        $row['duration'] = formatDuration($row['started_at'], new DateTime());
         $currentActivity = $row;
     } else {
         $row['finished_at'] = new DateTime($row['finished_at']);
+        $row['duration'] = formatDuration($row['started_at'], $row['finished_at']);
     }
     $activities[] = $row;
 }
@@ -66,13 +77,26 @@ foreach ($pdo->query('SELECT * FROM activity ORDER BY started_at DESC') as $row)
             .activity-list {
                 border-spacing: 0.5em 0.5em;
                 border-collapse: separate;
+                width: 100%;
+            }
+            .activity-list-title-column {
+                width: 99%;
+            }
+            .activity-list-duration-column {
+                white-space: nowrap;
+                text-align: right;
             }
         </style>
     </head>
     <body>
 <?php if (isset($currentActivity)): ?>
     <form method="POST" action="stop_activity.php" class="current-activity-display">
-        <h1><?= htmlspecialchars($currentActivity['title']) ?></h1>
+        <h1>
+            <?= htmlspecialchars($currentActivity['title']) ?>
+            <span data-start-time="<?= $currentActivity['started_at']->format(DateTime::ISO8601) ?>">
+                <?= $currentActivity['duration'] ?>
+            </span>
+        </h1>
         <input type="hidden" name="id" value="<?= $currentActivity['id'] ?>">
         <button type="submit">Stop</button>
     </form>
@@ -103,12 +127,32 @@ foreach ($pdo->query('SELECT * FROM activity ORDER BY started_at DESC') as $row)
         <td><?= $activity['started_at']->format('H:i') ?></td>
         <td>-</td>
         <td><?php if ($activity['finished_at']): ?><?= $activity['finished_at']->format('H:i') ?><?php endif; ?></td>
-        <td><?= htmlspecialchars($activity['title']) ?></td>
+        <td class="activity-list-title-column"><?= htmlspecialchars($activity['title']) ?></td>
+        <td class="activity-list-duration-column"<?php if ($activity === $currentActivity): ?> data-start-time="<?= $activity['started_at']->format(DateTime::ISO8601) ?>"<?php endif; ?>><?= $activity['duration'] ?></td>
     </tr>
 <?php endforeach; ?>
 <?php if (count($activities) > 0): ?>
             </tbody>
         </table>
 <?php endif; ?>
+        <script>
+            function formatDuration(start, end) {
+                var secs = Math.floor((end - start) / 1000);
+                var hours = Math.floor(secs / (60 * 60));
+                var mins = Math.floor(secs / 60) - hours * 60;
+                secs %= 60;
+                if (hours === 0) {
+                    return mins === 0 ? secs + 's' : mins + 'min';
+                } else {
+                    return hours + 'h ' + mins + 'min';
+                }
+            }
+            var durationElems = document.querySelectorAll('[data-start-time]');
+            setInterval(function() {
+                for (var i = 0, l = durationElems.length; i < l; i++) {
+                    durationElems[i].textContent = formatDuration(Date.parse(durationElems[i].dataset.startTime), Date.now());
+                }
+            }, 1000);
+        </script>
     </body>
 </html>
