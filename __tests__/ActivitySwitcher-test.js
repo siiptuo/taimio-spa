d9a77e9fb08eaace5dec00375eaf75316cebfb1a
Tuomas Siipola <siiptuo@kapsi.fi>
2016-05-13 00:04:52 +0300

    Utilize Enzyme in React component tests

--- __tests__/ActivitySwitcher-test.js
+++ __tests__/ActivitySwitcher-test.js
@@ -4,38 +4,51 @@ jest.dontMock('../src/activity');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 
 const ActivitySwitcher = require('../src/ActivitySwitcher').default;
 
 describe('ActivitySwitcher', () => {
-    it('displays normally', () => {
-        const renderer = ReactTestUtils.createRenderer();
-        renderer.render(<ActivitySwitcher loading={false} />);
-        const result = renderer.getRenderOutput();
+    function renderComponent(loading = true, onActivityStart = jest.genMockFunction()) {
+        return shallow(
+            <ActivitySwitcher
+                onActivityStart={onActivityStart}
+                loading={loading}
+            />
+        );
+    }
 
-        expect(result.type).toBe('form');
-        expect(result.props.className).toBe('start-activity-form');
-        expect(result.props.children).toEqual([
-            <input type="text"
-                placeholder="Activity title #tag1 #tag2"
-                value=""
-                onChange={result.props.children[0].props.onChange}
-                disabled={false}
-            />,
-            <button type="submit" disabled={false}>Start</button>
-        ]);
+    it('is a form with correct class', () => {
+        expect(renderComponent().is('form.start-activity-form')).toBe(true);
     });
 
-    it('handles click', () => {
-        const mockActivityStart = jest.genMockFunction();
-        const activitySwitcher = ReactTestUtils.renderIntoDocument(
-            <ActivitySwitcher loading={false} onActivityStart={mockActivityStart} />
-        );
+    it('contains an activity input', () => {
+        const input = renderComponent(false).find('input');
+        expect(input.length).toBe(1);
+        expect(input.prop('value')).toBe('');
+        expect(input.prop('placeholder')).toBe('Activity title #tag1 #tag2');
+        expect(input.prop('disabled')).toBe(false);
+    });
 
-        const input = ReactTestUtils.findRenderedDOMComponentWithTag(activitySwitcher, 'input');
-        input.value = 'Hello World #tiima';
-        ReactTestUtils.Simulate.change(input);
-        ReactTestUtils.Simulate.submit(ReactDOM.findDOMNode(activitySwitcher));
+    it('contains a start button', () => {
+        const button = renderComponent(false).find('button');
+        expect(button.length).toBe(1);
+        expect(button.text()).toBe('Start');
+        expect(button.prop('disabled')).toBe(false);
+    });
+
+    it('disables interaction when loading', () => {
+        const wrapper = renderComponent(true);
+        expect(wrapper.find('input').prop('disabled')).toBe(true);
+        expect(wrapper.find('button').prop('disabled')).toBe(true);
+    });
+
+    it('creates a new activity on button click', () => {
+        const mockActivityStart = jest.genMockFunction();
+        const wrapper = renderComponent(false, mockActivityStart);
+
+        wrapper.find('input').simulate('change', { target: { value: 'Hello World #tiima' } });
+        wrapper.simulate('submit', { preventDefault: () => {} });
 
         expect(mockActivityStart).toBeCalled();
         const activity = mockActivityStart.mock.calls[0][0];
@@ -43,7 +56,15 @@ describe('ActivitySwitcher', () => {
         expect(activity.tags).toEqual(['tiima']);
         // expect(activity.started_at).toBe();
         expect(activity.finished_at).toBeNull();
+    });
 
-        expect(input.value).toBe('');
+    it('does not accept empty input', () => {
+        const mockActivityStart = jest.genMockFunction();
+        const wrapper = renderComponent(false, mockActivityStart);
+
+        wrapper.find('input').simulate('change', { target: { value: ' ' } });
+        wrapper.simulate('submit', { preventDefault: () => {} });
+
+        expect(mockActivityStart.mock.calls.length).toBe(0);
     });
 });
