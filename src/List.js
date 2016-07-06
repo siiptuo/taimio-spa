d64b3b57c3f7ba3be1d4fe7eee8a551485a1cb6a
Tuomas Siipola <siiptuo@kapsi.fi>
2016-07-07 00:11:51 +0300

    Add basic search to activity list

--- src/List.js
+++ src/List.js
@@ -2,20 +2,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchActivitiesIfNeeded } from './actions';
-
+import { parseInput } from './activity';
 import ActivitySummary from './ActivitySummary';
 
 class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: '', tags: [] };
+        this.handleSearchInput = this.handleSearchInput.bind(this);
+    }
+
     componentDidMount() {
         this.props.dispatch(fetchActivitiesIfNeeded());
     }
 
+    handleSearchInput(event) {
+        this.setState(parseInput(event.target.value));
+    }
+
     render() {
+        const activities = this.props.activities
+            .filter(activity => activity.title.includes(this.state.title) &&
+                    this.state.tags.every(tag => activity.tags.includes(tag)));
         return (
-            <ActivitySummary
-                activities={this.props.activities}
-                loading={this.props.loading}
-            />
+            <div>
+                <div className="search-container">
+                    <input
+                        type="search"
+                        placeholder="Search activities by title and tags..."
+                        value={this.state.search}
+                        onInput={this.handleSearchInput}
+                    />
+                </div>
+                <ActivitySummary
+                    activities={activities}
+                    loading={this.props.loading}
+                />
+            </div>
         );
     }
 }
