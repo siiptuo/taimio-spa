59611130965a181ed3cfd1daff19b5dc2f858e0e
Tuomas Siipola <siiptuo@kapsi.fi>
2018-01-18 22:53:20 +0200

    Update Jest and remove old tests

--- webpack.common.js
+++ webpack.common.js
@@ -25,13 +25,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['env', 'react'],
-            plugins: [
-              'transform-object-rest-spread',
-              'transform-class-properties',
-            ],
-          },
         },
       },
       {
